974c9edf349ac5f2da924761ca9c3c3a8f95b15d
Unintelligible Maker <maker@unintelligiblemaker.com>
2026-01-31 12:32:34 -0800

    Update 'g' character data and refine example script; - Updated path and width for 'g' in characters_data.py; - Adjusted spiral turns and text scaling in Example.py; - Refined line spacing and text layout in Example.py

--- Example.py
+++ Example.py
@@ -14,26 +14,25 @@ def main():
     sandskrit = SandSkrit(0, 0)
 
     # Start with a clean slate by spiraling out to a radius of 100
-    sandskrit.add_spiral_out(radius=100, turns=1, ending_angle=0)
+    sandskrit.add_spiral_out(radius=100, turns=100, ending_angle=0)
     
     # Define multiple lines of text with varying scales to demonstrate size variation
     multi_lines = [
         {'text': 'Welcome to', 'scale': 10},
-        {'text': 'SandSkrit', 'scale': 20},
-        {'text': 'A python library for ', 'scale': 10},
+        {'text': 'SandSkrit', 'scale': 19},
+        {'text': 'A python library for', 'scale': 10},
         {'text': 'continuous path text', 'scale': 10},
         {'text': 'for sand tables.', 'scale': 10},
-        {'text': 'Turn your text into #', 'scale': 10},
-        # {'text': '#', 'scale': 50},
-        {'text': 'SandSkript', 'scale': 15},
-        {'text': 'by', 'scale': 8},
-        {'text': 'Unintelligible', 'scale': 12},
-        {'text': '    Maker', 'scale': 12},
+        {'text': 'Turn your text into', 'scale': 10},
+        # {'text': 'ogc', 'scale': 50, 'debug': True},
+        {'text': 'SandSkript', 'scale': 20},
+        {'text': 'by                  ', 'scale': 8},
+        {'text': 'Unintelligible', 'scale': 10},
+        {'text': '    Maker', 'scale': 10},
     ]
     
     try:
-        # Add the defined lines of text with a specific line spacing
-        sandskrit.add_lines_of_text(multi_lines, line_spacing=1.2)
+        sandskrit.add_lines_of_text(multi_lines, line_spacing=1.3)
     except ValueError as e:
         print(f"Oops, the sand would leak: {e}")
         return
